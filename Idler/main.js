f549093f522aae2b46d0f7447925f597370acf3a
YuriVitch <Yurivitch@hotmail.com>
2023-01-30 09:53:57 -0500

    Adds grinders and changes some variables

--- Idler/main.js
+++ Idler/main.js
@@ -1,8 +1,10 @@
 var money = 0;
 var scrap = 0;
 var scrappers = 0;
+var grinders = 0;
 var exchangerate = 1;
-var nextcost = 10
+var nextScrapperCost = 10;
+var nextGrinderCost = 50;
 
 function incrimentScrap(number){
 	scrap = scrap + number
@@ -15,7 +17,7 @@ function sellScrap(){
 		document.getElementById('Money').innerHTML = money;
 		document.getElementById('Scrap').innerHTML = scrap
 	}
-}
+};
 
 function buyScrapper(){
 	var scrapperCost = Math.floor(10 * Math.pow(1.2,scrappers));
@@ -25,16 +27,29 @@ function buyScrapper(){
 		document.getElementById('Scrappers').innerHTML = scrappers;
 		document.getElementById('Money').innerHTML = money
 		};
-	nextCost = Math.floor(10 * Math.pow(1.2, scrappers));
-	document.getElementById("Cost").innerHTML = nextCost;
+	nextScrapperCost = Math.floor(10 * Math.pow(1.2, scrappers));
+	document.getElementById("ScrapperCost").innerHTML = nextScrapperCost;
 };
 
+function buyGrinder(){
+	var grinderCost = Math.floor(50 * Math.pow(1.2,grinders))
+	if(money >= grinderCost){
+		grinders = grinders + 1;
+		money = money - grinderCost
+		document.getElementById("Grinders").innerHTML = grinders
+		document.getElementById("Money").innerHTML = money
+	};
+	nextGrinderCost = Math.floor(50 * Math.pow(1.2, grinders))
+	document.getElementById("GrinderCost").innerHTML = nextGrinderCost
+}
+	
 function save() {
 	var save = {
 		money: money,
-		scrappers: scrappers,
-		sCost: nextCost,
 		scrap: scrap,
+		scrappers: scrappers,
+		grinders: grinders,
+		sCost: nextScrapperCost,
 		rate: exchangerate
 	};
 	localStorage.setItem("save", JSON.stringify(save));
@@ -45,10 +60,14 @@ function load() {
 			money = savegame.money;
 			document.getElementById('Money').innerHTML = money
 		}
-				if(typeof savegame.printers !== "undefined"){
-			printers = savegame.printers;
-			document.getElementById('Printers').innerHTML = printers;
+				if(typeof savegame.scrappers !== "undefined"){
+			scrappers = savegame.scrappers;
+			document.getElementById('Scrappers').innerHTML = scrappers;
 	}
+			if(typeof savegame.grinders !== "undefined"){
+			grinders = savegame.grinders;
+			document.getElementById('Grinders').innerHTML = grinders;
+		}
 		if(typeof savegame.scrap !== "undefined"){
 			scrap = savegame.scrap;
 			document.getElementById('Scrap').innerHTML = scrap
@@ -56,6 +75,7 @@ function load() {
 		if(typeof savegame.rate !== "undefined"){
 			rate = savegame.rate;
 		}
+		console.log("Save Loaded")
 }
 		catch{console.log("So no save?")}
 }
@@ -65,4 +85,5 @@ function deleteSave(){
 
 window.setInterval(function(){
 	incrimentScrap(scrappers)
+	incrimentScrap(grinders * 5)
 }, 1000);
